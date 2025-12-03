9323e8509de319450da2bd5ffa23518007b15ef4
magicheng0816 <magicheng0816@163.com>
2025-12-03 20:44:28 +0800

    feat: add xllm header, fix log style, etc.

--- xllm/cc_api/llm.cpp
+++ xllm/cc_api/llm.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* Copyright 2025 The xLLM Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,8 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-=======
->>>>>>> 27678e1 (feat: add calling xllm through the so form.)
 #include "llm.h"
 
 #include <folly/Unit.h>
